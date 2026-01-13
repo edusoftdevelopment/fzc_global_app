2b786100f8d866b13c1371357f1171d239ab32c5
edusoftdevelopment <edusoftdevelopment@gmail.com>
2026-01-13 17:24:05 +0500

    done

--- lib/widgets/box_dispatched_card.dart
+++ lib/widgets/box_dispatched_card.dart
@@ -13,58 +13,28 @@ class BoxDispatchedCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-            colors: [
-              Colors.white,
-              Colors.grey.shade50,
-            ],
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.08),
-              blurRadius: 12,
-              offset: const Offset(0, 4),
-            ),
-          ],
-        ),
-        child: Stack(
-          children: [
-            // Serial number badge in top-right corner
-            Positioned(
-              top: -8,
-              right: -8,
-              child: Container(
-                width: 46,
-                height: 46,
-                decoration: BoxDecoration(
-                  color: Constants.primaryColor,
-                  shape: BoxShape.circle,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Constants.primaryColor.withOpacity(0.4),
-                      blurRadius: 8,
-                      offset: const Offset(0, 2),
-                    ),
-                  ],
-                ),
-                alignment: Alignment.center,
-                child: Text(
-                  '${index + 1}',
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                  ),
-                ),
+      child: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(16),
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.white,
+                  Colors.grey.shade50,
+                ],
               ),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.08),
+                  blurRadius: 12,
+                  offset: const Offset(0, 4),
+                ),
+              ],
             ),
-            // Main content
-            Padding(
+            child: Padding(
               padding: const EdgeInsets.all(16),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -171,12 +141,224 @@ class BoxDispatchedCard extends StatelessWidget {
                 ],
               ),
             ),
-          ],
-        ),
+          ),
+          // Serial number badge in top-right corner
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Container(
+              width: 40,
+              height: 40,
+              decoration: BoxDecoration(
+                color: Constants.primaryColor,
+                borderRadius: const BorderRadius.only(
+                  topRight: Radius.circular(16),
+                  bottomLeft: Radius.circular(16),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Constants.primaryColor.withOpacity(0.4),
+                    blurRadius: 8,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                '${index + 1}',
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+// import 'package:fzc_global_app/models/box_dispatched_status_model.dart';
+// import 'package:fzc_global_app/utils/constants.dart';
+
+// class BoxDispatchedCard extends StatelessWidget {
+//   final Dt item;
+//   final int index;
+
+//   const BoxDispatchedCard({required this.item, required this.index, Key? key})
+//       : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+//       child: Container(
+//         decoration: BoxDecoration(
+//           borderRadius: BorderRadius.circular(16),
+//           gradient: LinearGradient(
+//             begin: Alignment.topLeft,
+//             end: Alignment.bottomRight,
+//             colors: [
+//               Colors.white,
+//               Colors.grey.shade50,
+//             ],
+//           ),
+//           boxShadow: [
+//             BoxShadow(
+//               color: Colors.black.withOpacity(0.08),
+//               blurRadius: 12,
+//               offset: const Offset(0, 4),
+//             ),
+//           ],
+//         ),
+//         child: Stack(
+//           children: [
+//             // Serial number badge in top-right corner
+//             Positioned(
+//               top: -8,
+//               right: -8,
+//               child: Container(
+//                 width: 46,
+//                 height: 46,
+//                 decoration: BoxDecoration(
+//                   color: Constants.primaryColor,
+//                   shape: BoxShape.circle,
+//                   boxShadow: [
+//                     BoxShadow(
+//                       color: Constants.primaryColor.withOpacity(0.4),
+//                       blurRadius: 8,
+//                       offset: const Offset(0, 2),
+//                     ),
+//                   ],
+//                 ),
+//                 alignment: Alignment.center,
+//                 child: Text(
+//                   '${index + 1}',
+//                   style: const TextStyle(
+//                     color: Colors.white,
+//                     fontWeight: FontWeight.bold,
+//                     fontSize: 14,
+//                   ),
+//                 ),
+//               ),
+//             ),
+//             // Main content
+//             Padding(
+//               padding: const EdgeInsets.all(16),
+//               child: Column(
+//                 crossAxisAlignment: CrossAxisAlignment.start,
+//                 children: [
+//                   // Barcode with icon
+//                   Row(
+//                     children: [
+//                       Icon(
+//                         Icons.barcode_reader,
+//                         color: Constants.primaryColor,
+//                         size: 20,
+//                       ),
+//                       const SizedBox(width: 8),
+//                       Expanded(
+//                         child: Text(
+//                           item.barcode ?? '',
+//                           style: const TextStyle(
+//                             fontWeight: FontWeight.bold,
+//                             fontSize: 16,
+//                             letterSpacing: 0.5,
+//                           ),
+//                           maxLines: 1,
+//                           overflow: TextOverflow.ellipsis,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                   const SizedBox(height: 12),
+//                   // Customer info
+//                   Container(
+//                     padding: const EdgeInsets.symmetric(
+//                       vertical: 8,
+//                       horizontal: 12,
+//                     ),
+//                     decoration: BoxDecoration(
+//                       color: Constants.primaryColor.withOpacity(0.08),
+//                       borderRadius: BorderRadius.circular(8),
+//                     ),
+//                     child: Row(
+//                       children: [
+//                         Icon(
+//                           Icons.person_outline,
+//                           size: 16,
+//                           color: Constants.primaryColor,
+//                         ),
+//                         const SizedBox(width: 6),
+//                         Expanded(
+//                           child: Text(
+//                             item.customerName ?? '',
+//                             style: TextStyle(
+//                               fontSize: 13,
+//                               color: Colors.grey.shade800,
+//                             ),
+//                             maxLines: 1,
+//                             overflow: TextOverflow.ellipsis,
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ),
+//                   const SizedBox(height: 10),
+//                   // Delivery mode and time
+//                   Row(
+//                     children: [
+//                       Expanded(
+//                         child: Row(
+//                           children: [
+//                             Icon(
+//                               Icons.location_on_outlined,
+//                               size: 16,
+//                               color: Colors.grey.shade600,
+//                             ),
+//                             const SizedBox(width: 4),
+//                             Expanded(
+//                               child: Text(
+//                                 item.deliveryModeTitle ?? '',
+//                                 style: TextStyle(
+//                                   fontSize: 12,
+//                                   color: Colors.grey.shade600,
+//                                 ),
+//                                 maxLines: 1,
+//                                 overflow: TextOverflow.ellipsis,
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                       ),
+//                       const SizedBox(width: 8),
+//                       Icon(
+//                         Icons.schedule,
+//                         size: 16,
+//                         color: Colors.grey.shade600,
+//                       ),
+//                       const SizedBox(width: 4),
+//                       Text(
+//                         item.formattedDispatchedTime ?? '',
+//                         style: TextStyle(
+//                           fontSize: 12,
+//                           color: Colors.grey.shade600,
+//                         ),
+//                       ),
+//                     ],
+//                   ),
+//                 ],
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
 // import 'package:flutter/material.dart';
 // import 'package:fzc_global_app/models/box_dispatched_status_model.dart';
